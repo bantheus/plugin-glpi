5368c82e04e58baf92d076a48bed728259723432
bantheus <matheuschmidt.b@gmail.com>
2025-06-25 12:11:18 -0300

    feat: add initial setup for Informações Adicionais plugin

---
+++ glpi_data/glpi/plugins/aditionalinfo/setup.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Plugin Informações Adicionais para Chamados - Teste PLSS
+ */
+
+define('PLUGIN_ADITIONALINFO_VERSION', '1.0.0');
+
+/**
+ * Inicializa o plugin e define os hooks necessários.
+ */
+function plugin_init_aditionalinfo(): void
+{
+  global $PLUGIN_HOOKS;
+
+  $PLUGIN_HOOKS['csrf_compliant']['aditionalinfo'] = true;
+}
+
+/**
+ * Informações do plugin.
+ */
+function plugin_version_aditionalinfo(): array
+{
+  return [
+    'name' => 'Plugin Informações Adicionais para Chamados',
+    'version' => PLUGIN_ADITIONALINFO_VERSION,
+    'author' => 'Matheus Schmidt',
+    'license' => 'GPLv2+',
+    'homepage' => '',
+    'requirements' => [
+      'glpi' => [
+        'min' => '10.0',
+      ]
+    ]
+  ];
+}
+
+/**
+ * Verifica os pré-requisitos do plugin.
+ */
+function plugin_aditionalinfo_check_prerequisites(): bool
+{
+  if (version_compare(GLPI_VERSION, '10.0', 'lt')) {
+    echo "Este plugin requer GLPI 10.0 ou superior.";
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Verifica a configuração do plugin.
+ */
+function plugin_aditionalinfo_check_config($verbose = false): bool
+{
+  global $DB;
+
+  $plugin = new Plugin();
+  if (!$plugin->isInstalled('aditionalinfo')) {
+    if ($verbose) {
+      echo "Plugin Informações Adicionais não está instalado corretamente.";
+    }
+    return false;
+  }
+
+  if (!$plugin->isActivated('aditionalinfo')) {
+    if ($verbose) {
+      echo "Plugin Informações Adicionais não está ativado.";
+    }
+    return false;
+  }
+
+  $plugin_dir = GLPI_ROOT . "/plugins/aditionalinfo";
+  if (!is_readable($plugin_dir)) {
+    if ($verbose) {
+      echo "Diretório do plugin não possui permissões de leitura adequadas.";
+    }
+    return false;
+  }
+
+  return true;
+}
+?>
